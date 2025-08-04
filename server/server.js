0ff056302f80bc1af99a335fb912be47cb30cadb
AnythingOP <p.d.b812783@gmail.com>
2025-08-04 15:57:57 +0530

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -15,6 +15,30 @@ app.use(cors());
 
 app.use(express.json());
 
+// server/server.js
+
+// ... other imports
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// START: ADD THIS DEBUGGING CODE
+app.get('/debug-connection', (req, res) => {
+    const connectionState = mongoose.connection.readyState;
+    // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+    res.json({
+        state: connectionState,
+        dbName: mongoose.connection.name,
+        host: mongoose.connection.host,
+        port: mongoose.connection.port
+    });
+});
+// END: ADD THIS DEBUGGING CODE
+
+mongoose.connect(process.env.MONGO_URI)
+// ... rest of the file
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
